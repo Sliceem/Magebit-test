d96927b8e833d330b344409f4f32e648879296e1
sliceem88 <sliceem88@gmail.com>
2020-04-19 20:07:29 +0300

    Add New user to DB + Chech if user exists

--- Core/DB.php
+++ Core/DB.php
@@ -2,33 +2,50 @@
 
 require_once 'Config.php';
 
-class Db
+class DB
 {
     private $pdo;
     private static $instance = null;
 
-    private function __construct(){
-        $dsn = "mysql:host=".HOST.";dbname=".DBNAME;
-        try{
+    private function __construct()
+    {
+        $dsn = "mysql:host=" . HOST . ";dbname=" . DBNAME;
+        try {
             $this->pdo = new PDO($dsn, DBLOGIN, DBPASS);
             $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-        } catch (PDOException $err){
-            throw new PDOException($err->getMessage());
+        } catch (PDOException $err) {
+            echo 'Error accured: ' . $err->getMessage() . BR;
+            die();
         }
     }
 
-    public static function instance(){
-        if(!isset(self::$instance)){
-            self::$instance == new DB;
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new DB();
         }
         return self::$instance;
     }
 
-    public function chechUniqEmail(){
-        $sql = "SELECT * FROM users";
+    public function chechUniqEmail($email)
+    {
+        $sql = "SELECT * FROM users WHERE user_email = '$email'";
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute();
-        // $emailCount = $stmt->rowCount();
-        // return $emailCount;
+        $emailCount = $stmt->rowCount();
+        return $emailCount;
+    }
+
+    public function addNewUser($data)
+    {
+        $columns = rtrim(implode(',', array_keys($data)), ',');
+        $sql = "INSERT INTO users ($columns) VALUES (?,?,?)";
+        $stmt = $this->pdo->prepare($sql);
+        $x = 1;
+        foreach ($data as $key => $value) {
+            $stmt->bindValue($x, $value);
+            $x++;
+        }
+        $stmt->execute();
     }
 }
--- Model/Register.Model.php
+++ Model/Register.Model.php
@@ -9,36 +9,54 @@ class Register
     private $password;
     private $error = [];
 
-    public function __construct($data){
-        $this->username = filter_var($data['username'],FILTER_SANITIZE_STRING);
+    public function __construct($data)
+    {
+        $this->username = filter_var($data['username'], FILTER_SANITIZE_STRING);
         $this->email = $data['email'];
         $this->password = filter_var($data['password'], FILTER_SANITIZE_STRING);
     }
 
-    public function signup(){
-        if ($this->validateEmail() && $this->validateUsername() && $this->validatePassword()){
-            // return $this->validateEmail() . ' - ' . $this->validateUsername() . ' - ' . $this->validatePassword();
-            $var = DB::instance()->chechUniqEmail();
-            return $var;
-        }else return $this->error;
+    public function signup()
+    {
+        if ($this->validateEmail() && $this->validateUsername() && $this->validatePassword()) {
+            if ($this->checkEmailInDB()) {
+                $data = [
+                    'user_name'     => $this->validateUsername(),
+                    'user_email'    => $this->validateEmail(),
+                    'user_password' => $this->validatePassword()
+                ];
+                DB::getInstance()->addNewUser($data);
+            } else return $this->error;
+        } else return $this->error;
     }
 
-    private function validateEmail(){
-        if(filter_var($this->email, FILTER_VALIDATE_EMAIL)){
+    private function validateEmail()
+    {
+        if (filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
             return $this->email;
-        }else $this->error['email'] = 'Email not valid, try once more';
+        } else $this->error['email'] = 'Email not valid, try once more';
     }
 
-    private function validateUsername(){
-        if(strlen($this->username) > 4  && strlen($this->username) < 15){
+    private function validateUsername()
+    {
+        if (strlen($this->username) > 4  && strlen($this->username) < 15) {
             return $this->username;
-        }else $this->error['username'] = 'Username must be more than 4 and less than 15 symbols';
+        } else $this->error['username'] = 'Username must be more than 4 and less than 15 symbols';
     }
 
-    private function validatePassword(){
-        if(strlen($this->password)> 6){
+    private function validatePassword()
+    {
+        if (strlen($this->password) > 6) {
             return password_hash($this->password, PASSWORD_DEFAULT);
-        }else $this->error['password'] = 'Password must be more than 6 symbols';
+            // return $this->password;
+        } else $this->error['password'] = 'Password must be more than 6 symbols';
     }
 
+    public function checkEmailInDB()
+    {
+        $result = DB::getInstance()->chechUniqEmail($this->validateEmail());
+        if ($result == 0) {
+            return true;
+        } else $this->error['user_exists'] = 'User With this Email Already Exists!';
+    }
 }
